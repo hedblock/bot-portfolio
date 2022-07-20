1d9023a20de89fe6599ee15e4d7adf71c3b29f66
jason <jason@hedmans.org>
2022-07-20 17:03:18 -0400

    clean cloud functions

--- cloud/submissionCache.js
+++ cloud/submissionCache.js
@@ -36,44 +36,4 @@ Moralis.Cloud.job("cacheSubmissionWeekly", async (request) => {
 
     return true;
 
-})
-
-Moralis.Cloud.define("cacheSubmissionWeekly", async (_) => {
-
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
-    const lastSunday = new Date(today.setDate(today.getDate() - today.getDay()));
-
-    const Results = Moralis.Object.extend("Results");
-    const resultsQuery = new Moralis.Query(Results);
-    resultsQuery.equalTo("startDate", lastSunday);
-    const results = await resultsQuery.find();
-    if(results.length > 0) return false;
-
-    const Submissions = Moralis.Object.extend("Submissions");
-    const query = new Moralis.Query(Submissions);
-    query.greaterThan("updatedAt", lastSunday);
-    const submissions = await query.find();
-
-    const resultsAggregate = submissions.reduce((acc, submission) => {
-        submission.get('allocations').forEach(allocation => {
-            acc[allocation.token] = (acc[allocation.token] || 0) + allocation.allocation;
-        })
-        return acc;
-    }, {});
-
-    const resultsNormalized = Object.keys(resultsAggregate).reduce((acc, token) => {
-        acc[token] = resultsAggregate[token] / submissions.length;
-        return acc;
-    }, resultsAggregate);
-
-    const Result = Moralis.Object.extend("Results");
-    const result = new Result();
-    result.set("submissions", submissions.length);
-    result.set("startDate", lastSunday);
-    result.set("results", resultsNormalized);
-    await result.save();
-
-    return true;
-
-})
+});
