fa4ec5a6769043787a6b9866f339404bd60f6300
jason <jason@hedmans.org>
2022-07-28 11:45:01 -0400

    round allocation change

--- hooks/useResults.ts
+++ hooks/useResults.ts
@@ -64,7 +64,7 @@ const useResults = () => {
             return {
                 ...token,
                 allocation: currentAllocation,
-                change: currentAllocation - lastAllocation,
+                change: round2(currentAllocation - lastAllocation),
             }
         })
         .sort((a, b) => b.allocation - a.allocation);
